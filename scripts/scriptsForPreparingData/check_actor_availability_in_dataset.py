9e273116c55f1f11f717059062c694cb3675f306
Andrew Cherevko <cherevkin@gmail.com>
2019-04-10 23:51:04 +0200

    finilized version of check_actor

--- scripts/scriptsForPreparingData/check_actor_availability_in_dataset.py
+++ scripts/scriptsForPreparingData/check_actor_availability_in_dataset.py
@@ -4,7 +4,7 @@ import re
 
 default_input_file_path = '../../staggingArea/parsed_credits.csv.json'
 
-#
+# Uncomment when rns create_JSON_from_CSV script (data doesn`t exist)
 # JSON_raw = open(default_input_file_path, 'r').readlines()
 # JSON_object = json.loads(JSON_raw[0])
 prepared_actors = open('../../actors', 'r').read().split('\n')
@@ -34,15 +34,26 @@ def check_actor_infilm_JSON():
     print('Unique actors count:', len(unique_actors))
 
 
-# check_actor_infilm_JSON()
-
-
-
-
-
-
 def return_new_row(old_row, producer_cell, actor_cell):
-    #init array
+    """
+    return_new_row Fuction return new immtable row
+
+    [Immutability its one of the most needed filar in programing, 
+    we used this function for immutability of our data it means,
+     create new data and return it, rather then reasign variables, 
+     what can be a source of bugs and perfomence issues]
+
+    :param old_row: [old row from dataset]
+    :type old_row: [array]
+    :param producer_cell: [cell with producer name]
+    :type producer_cell: [String]
+    :param actor_cell: [cell with actor name]
+    :type actor_cell: [String]
+    :return: [new immutable row]
+    :rtype: [Array]
+    """
+
+    # init array
     new_row = [None] * 4
     film_id = old_row[2]
     new_row[0] = old_row[0]
@@ -54,6 +65,18 @@ def return_new_row(old_row, producer_cell, actor_cell):
 
 
 def create_proper_JSON(bad_formated_cell):
+    '''
+    create_proper_JSON This function create proper JSON from bad formated string
+    
+    [In our dataset data called "JSON" wasn`t really javascript notation object, we can call it "bad JSON" or badly fomated,
+    for futher use of our dataset we was needed to transofm from "bad JSON" to array of properly formated String aka "raw JSON"]
+    
+    :param bad_formated_cell: [cell with bad formated data]
+    :type bad_formated_cell: [String]
+    :return: [properly formated String]
+    :rtype: [String]
+    '''
+
     open_brackets_replaced = bad_formated_cell.replace("{\'", "{\"")
     close_brackets_replaced = open_brackets_replaced.replace("\'}", "\"}")
     space_plus_sinqle_quote_replaced = close_brackets_replaced.replace(
@@ -66,15 +89,23 @@ def create_proper_JSON(bad_formated_cell):
     return cell_with_proper_JSON
 
 
-
 def create_CSV_header(number_of_columns, *args):
-    CSV_header=[None] * number_of_columns
+    '''
+    create_CSV_header creates header for CSV file
+
+    Using this function we can create CSV header with custom number of cols and names
+
+    :param number_of_columns: define number of columns in row 
+    :type number_of_columns: [int]
+    :return: [Array of columns]
+    :rtype: [Array of Strings]
+    '''
+
+    CSV_header = [None] * number_of_columns
     for id, column_name in enumerate(args):
-        CSV_header[id]=column_name
+        CSV_header[id] = column_name
 
     return CSV_header
-
-
 
 
 def check_actor_in_film_CSV():
@@ -94,12 +125,12 @@ def check_actor_in_film_CSV():
     actors_in_a_films = []
     unique_actors = []
     iteration = 0
-    #init output file
+    # init output file
     fout = open('../..//helpers/outfile.csv', 'w', encoding="utf8")
     with open('../../originalDataset/credits.csv', 'r', encoding="utf8") as fin:
         writer = csv.writer(fout)
-        #create and write header to csv file
-        header=create_CSV_header(4, "shit", "filmId", "producer", "actor")
+        # create and write header to csv file
+        header = create_CSV_header(4, "shit", "filmId", "producer", "actor")
         writer.writerow(header)
         for row in csv.reader(fin):
             for actor in prepared_actors:
@@ -110,14 +141,16 @@ def check_actor_in_film_CSV():
                         # modify "bad JSON" from CSV for proper parsing
                         properly_modified_to_JSON_cell = create_proper_JSON(
                             row[1])
-                        #parse to JSON
-                        array_of_JSON = json.loads(properly_modified_to_JSON_cell)
+                        # parse to JSON
+                        array_of_JSON = json.loads(
+                            properly_modified_to_JSON_cell)
                         for single_object in array_of_JSON:
                             if re.search("^Producer$", single_object['job']):
                                 # assign for better readability
                                 producer_cell = single_object['name']
                                 actors_in_a_films.append(actor)
-                                new_row = return_new_row(row, producer_cell, actor_cell)
+                                new_row = return_new_row(
+                                    row, producer_cell, actor_cell)
                                 writer.writerow(new_row)
                                 iteration += 1
                                 print(iteration, producer_cell, actor_cell)
@@ -131,3 +164,4 @@ def check_actor_in_film_CSV():
 
 
 check_actor_in_film_CSV()
+# check_actor_infilm_JSON()
