22256b70e66a71f2009ecaa27bf460871df64972
Andrew Cherevko <cherevkin@gmail.com>
2019-04-10 23:35:35 +0200

    more convinience refactoring of CSV funct with added new comments

--- scripts/scriptsForPreparingData/check_actor_availability_in_dataset.py
+++ scripts/scriptsForPreparingData/check_actor_availability_in_dataset.py
@@ -37,25 +37,8 @@ def check_actor_infilm_JSON():
 # check_actor_infilm_JSON()
 
 
-fout = open('../..//helpers/outfile.csv', 'w', encoding="utf8")
 
 
-def modify_CSV_by_adding_name_of_actor(row_to_modify, actor_name):
-    '''
-    modify_CSV_by_adding_name_of_actor Modify row from original dataset
-
-    Modify row by a new value (actor name)
-
-    :param row_to_modify: row number
-    :type row_to_modify: String
-    :param actor_name: Name of an actor
-    :type actor_name: String
-    :return: Modyfied row
-    :rtype: String
-    '''
-
-    row_to_modify = actor_name
-    return row_to_modify
 
 
 def return_new_row(old_row, producer_cell, actor_cell):
@@ -83,15 +66,26 @@ def create_proper_JSON(bad_formated_cell):
     return cell_with_proper_JSON
 
 
-test = open('./actorsInAFilms.txt', 'w')
+
+def create_CSV_header(number_of_columns, *args):
+    CSV_header=[None] * number_of_columns
+    for id, column_name in enumerate(args):
+        CSV_header[id]=column_name
+
+    return CSV_header
+
+
 
 
 def check_actor_in_film_CSV():
     '''
     check_actor_in_film_CSV Check if actor from prepared list is in a dataset
 
-
-    Check if actor from prepared list is in a dataset
+    Create output file and write custom header
+    Check if actor from prepared list is in a dataset, next
+    Run function for proper modifing "badJSON", after parse JSON
+    In the next step function using regexp takes from field Producer name and store it.
+    The main last step its create new row with needed for use data and write to a file
     Print count of films with our actor
     Print count of unique actors
 
@@ -100,8 +94,13 @@ def check_actor_in_film_CSV():
     actors_in_a_films = []
     unique_actors = []
     iteration = 0
+    #init output file
+    fout = open('../..//helpers/outfile.csv', 'w', encoding="utf8")
     with open('../../originalDataset/credits.csv', 'r', encoding="utf8") as fin:
         writer = csv.writer(fout)
+        #create and write header to csv file
+        header=create_CSV_header(4, "shit", "filmId", "producer", "actor")
+        writer.writerow(header)
         for row in csv.reader(fin):
             for actor in prepared_actors:
                 if actor in row[0]:
@@ -111,14 +110,13 @@ def check_actor_in_film_CSV():
                         # modify "bad JSON" from CSV for proper parsing
                         properly_modified_to_JSON_cell = create_proper_JSON(
                             row[1])
-
+                        #parse to JSON
                         array_of_JSON = json.loads(properly_modified_to_JSON_cell)
                         for single_object in array_of_JSON:
                             if re.search("^Producer$", single_object['job']):
+                                # assign for better readability
                                 producer_cell = single_object['name']
-
                                 actors_in_a_films.append(actor)
-
                                 new_row = return_new_row(row, producer_cell, actor_cell)
                                 writer.writerow(new_row)
                                 iteration += 1
